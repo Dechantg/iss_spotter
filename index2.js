d19c107ee1f378be300cfeea79605707e8bd9e27
Gregory Dechant <dechantg@gmail.com>
2023-09-29 03:52:03 +0000

    again i had to rely on the examples too much. I will need to go through all of these again this weekend to better understand it.

---
+++ index2.js
@@ -0,0 +1,25 @@
+
+const { nextISSTimesForMyLocation  } = require('./iss_promised');
+
+
+
+ const printPassTimes = function(passTimes) {
+    for (const pass of passTimes) {
+      const datetime = new Date(0);
+      datetime.setUTCSeconds(pass.risetime);
+      const duration = pass.duration;
+      console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+    }
+  };
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+
+ 
+// fetchMyIP()
+// .then(fetchCoordsByIP)
+// .then(fetchISSFlyOverTimes)
+// .then(body => console.log(body));
